f90b5f0dff32f52e16b19e4a64a35049ea646038
Roni Chen <92129708+RoniChen971@users.noreply.github.com>
2022-04-05 22:04:01 +0300

    Create PlusMinus.java

---
+++ PlusMinus.java
@@ -0,0 +1,12 @@
+public class PlusMinus {
+public static void plusAndMinus(int num1, int num2) {
+        plusMinus( num1, num2);
+    }
+
+    private static void plusMinus(int num1, int num2) {
+
+        System.out.println( num1 + " + " + num2+ " = "+ (num1+num2));
+        System.out.println( num1 + " - " + num2 +" = "+ (num1- num2))
+
+    }
+  }
